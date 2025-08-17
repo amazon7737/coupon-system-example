fb0357a012c56fefd09c8744bd76bc7da94e0f00
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-08-18 07:22:37 +0900

    redis set을 활용하여 중복처리 되는지 확인

--- api/src/main/java/org/example/api/service/ApplyService.java
+++ api/src/main/java/org/example/api/service/ApplyService.java
@@ -2,6 +2,7 @@ package org.example.api.service;
 
 import org.example.api.domain.Coupon;
 import org.example.api.producer.CouponCreateProducer;
+import org.example.api.repository.AppliedUserRepository;
 import org.example.api.repository.CouponCountRepository;
 import org.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -14,13 +15,22 @@ public class ApplyService {
 
     private final CouponCreateProducer couponCreateProducer;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
+    private final AppliedUserRepository appliedUserRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+        Long apply = appliedUserRepository.add(userId);
+
+        if(apply != 1) {
+            return;
+        }
+
         Long count = couponCountRepository.increment();
 
         if (count > 100) {
