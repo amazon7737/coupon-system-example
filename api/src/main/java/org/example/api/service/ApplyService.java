191dbb39d461789eadd90fdfef3a8df34164ff6d
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-08-18 06:08:54 +0900

    consumer 의 create 메서드로 변경

--- api/src/main/java/org/example/api/service/ApplyService.java
+++ api/src/main/java/org/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package org.example.api.service;
 
 import org.example.api.domain.Coupon;
+import org.example.api.producer.CouponCreateProducer;
 import org.example.api.repository.CouponCountRepository;
 import org.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,12 @@ public class ApplyService {
 
     private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+    private final CouponCreateProducer couponCreateProducer;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void apply(Long userId) {
@@ -23,6 +27,6 @@ public class ApplyService {
             return;
         }
 
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
