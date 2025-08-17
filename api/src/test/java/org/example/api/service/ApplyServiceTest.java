6c9e85616ca744990bb9c9e166f272ea487e2932
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-08-17 21:01:10 +0900

    1000명이 동시에 응모했을때의 요청 테스트 케이스 구성

--- api/src/test/java/org/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/org/example/api/service/ApplyServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
@@ -21,5 +24,21 @@ public class ApplyServiceTest {
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러명응모() {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        for(int i=0; i< threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                }finally {
+                }
+            });
+        }
     }
 }
